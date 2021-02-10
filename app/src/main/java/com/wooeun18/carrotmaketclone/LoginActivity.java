3373342a6004e9e9f8565d9857b9fa8fdfbd7ff0
woobin <hmy1219@naver.com>
2021-02-10 15:30:02 +0900

    dev2

--- app/src/main/java/com/wooeun18/carrotmaketclone/LoginActivity.java
+++ app/src/main/java/com/wooeun18/carrotmaketclone/LoginActivity.java
@@ -2,13 +2,35 @@ package com.wooeun18.carrotmaketclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText et;
+    CircleImageView civ;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        et= findViewById(R.id.et);
+        civ= findViewById(R.id.iv);
+
+
+
+    }
+
+    public void clickImage(View view) {
+
+    }
+
+    public void clickBtn(View view) {
     }
 }
